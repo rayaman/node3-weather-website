9b2baad8dc7e59ef3b34d746cdd9a4754401eb76
Ryan Ward <rayaman99@gmail.com>
2019-08-31 15:24:13 -0400

    Made required changes for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,15 @@ const weatherform = document.querySelector("form")
 const search = document.querySelector("input")
 const messageOne = document.querySelector("#p1")
 const messageTwo = document.querySelector("#p2")
+
 weatherform.addEventListener("submit", (e) => {
     e.preventDefault()
+
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ""
+
     const location = search.value
+
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
